356c5b9c9ee9c9e6861108415dd11814ce47e95e
Prabaesa Yudha Tama <prabaesa.yudha@salt.co.id>
2021-03-16 21:18:10 +0700

    Initial Commit

---
+++ TOG-TestTests/TOG_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  TOG_TestTests.swift
+//  TOG-TestTests
+//
+//  Created by Yudha on 16/03/21.
+//
+
+import XCTest
+@testable import TOG_Test
+
+class TOG_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
